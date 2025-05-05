4f49b42bbeda643a5070c50fc4d8bf512586d948
nsubbot <nataly.subbotina@gmail.com>
2025-05-05 14:07:10 +0300

    BasePage project initial commit

--- test_main_page.py
+++ test_main_page.py
@@ -2,8 +2,12 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
+def go_to_login_page(browser):
     login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
     login_link.click()
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+
+    browser.get(link)
+    go_to_login_page(browser)
