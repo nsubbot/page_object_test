85252ecaf806d96e532cb32e7264767cee717761
nsubbot <nataly.subbotina@gmail.com>
2025-05-06 11:26:56 +0300

    Test for product page has been added

--- pages/locators.py
+++ pages/locators.py
@@ -13,3 +13,13 @@ class LoginPageLocators():
     # REGISTER_CONFIRM_PASSWORD = (By.CSS_SELECTOR, "input#id_registration-password2")
 
     # REGISTER_SUBMIT_BUTTON = (By.CSS_SELECTOR, "button[name='registration_submit']")
+
+class ProductPageLocators():
+    ADD_TO_BASKET_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    PRODUCT_NAME = (By.CSS_SELECTOR, "div.product_main > h1")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, "div.product_main > p.price_color")
+
+    PRODUCT_NAME_IN_BASKET = (By.CSS_SELECTOR, "div#messages > div:nth-child(1) > div > strong")
+    PRODUCT_PRICE_IN_BASKET = (By.CSS_SELECTOR, "div#messages > div:nth-child(3) > div > p > strong")
+
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, ".alert-success")
---
+++ pages/product_page.py
@@ -0,0 +1,20 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+class ProductPage(BasePage):
+     def add_product_to_basket(self):
+         selected_product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+         selected_product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+     
+         ## add product to basket
+         self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON).click()
+        
+         ## get code
+         self.solve_quiz_and_get_code()
+
+         ## check basket
+         added_product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME_IN_BASKET).text
+         assert selected_product_name == added_product_name, "Added product name is not equal selected product name"
+
+         added_product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE_IN_BASKET).text
+         assert selected_product_price == added_product_price, "Added product price is not equal selected product price"
